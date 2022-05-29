291bc735f22f1828034898badc9e6539b2f8535b
HWaiChan <hanswai93@gmail.com>
2022-05-29 19:48:46 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+# This is going to be a basic hereustic for the model to benchmark its accuracy.
+# The most basic form of recommendation (for me) is to go to all animes and sort by Score
+# and go through the ones that I have no watched
+# sort:Score filter:watched maybe filter:genre.
